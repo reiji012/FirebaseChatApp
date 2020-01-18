d23511f86f9e4bc20558bec7ad6b252cf721c1e8
rejii <reiji0120124@gmail.com>
2020-01-18 17:33:12 +0900

    フォルダ修正

---
+++ FirebaseChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseChatApp
+//
+//  Created by reiji matsumura on 2020/01/18.
+//  Copyright © 2020 reiji matsumura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
